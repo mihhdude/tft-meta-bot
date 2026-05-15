c98974015a2b24ee25e48f91625747e7eed6a9c6
mihhdude <mihhdude@gmail.com>
2026-05-15 17:08:45 +0000

    debug sheet

--- update_meta.py
+++ update_meta.py
@@ -1,21 +1,15 @@
 import os
 import json
 import gspread
-
 from oauth2client.service_account import (
     ServiceAccountCredentials
 )
 
 scope = [
-    'https://spreadsheets.google.com/feeds',
-    'https://www.googleapis.com/auth/spreadsheets',
-    'https://www.googleapis.com/auth/drive'
+'https://spreadsheets.google.com/feeds',
+'https://www.googleapis.com/auth/spreadsheets',
+'https://www.googleapis.com/auth/drive'
 ]
-
-
-# -------------------------
-# Login Google
-# -------------------------
 
 creds = json.loads(
     os.environ["GOOGLE_CREDS"]
@@ -33,51 +27,11 @@ client = gspread.authorize(
     credentials
 )
 
-print("✅ Google auth OK")
-
-
-# -------------------------
-# Debug: xem service account
-# thấy những file nào
-# -------------------------
+print("Google auth OK")
 
 files = client.openall()
 
 print(
-    "Sheets nhìn thấy:",
-    [f.title for f in files]
+"Nhìn thấy:",
+[f.title for f in files]
 )
-
-
-# -------------------------
-# MỞ FILE GOOGLE SHEET
-# ĐỔI TÊN CHO ĐÚNG
-# -------------------------
-
-SHEET_NAME = "MetaTFT"
-
-try:
-
-    spreadsheet = client.open(
-        SHEET_NAME
-    )
-
-    sheet = spreadsheet.sheet1
-
-    sheet.update(
-        "A1",
-        [["hello"]]
-    )
-
-    print(
-        "✅ write ok"
-    )
-
-except Exception as e:
-
-    print(
-        "❌ lỗi:",
-        str(e)
-    )
-
-    raise
